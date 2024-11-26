402b2bce19410c12654e43fa2036a5c49f110d5a
fodorad <fodorad201@gmail.com>
2024-11-26 11:34:32 +0100

    Add dependency version

--- blinklinmult/__init__.py
+++ blinklinmult/__init__.py
@@ -1,12 +1,16 @@
 import toml
 from os import PathLike
 from pathlib import Path
+from importlib.metadata import version
+
+try:
+    __version__ = version("blinklinmult")
+except Exception:
+    __version__ = "unknown"
 
 # Module level constants
 PROJECT_ROOT = Path(__file__).parents[1]
 WEIGHTS_DIR = Path().home() / '.cache' / 'torch' / 'hub' / 'checkpoints' / 'blink'
-
-__version__ = toml.load(PROJECT_ROOT / 'pyproject.toml')['project']['version']
 
 # Type aliases
 PathType = str | PathLike
